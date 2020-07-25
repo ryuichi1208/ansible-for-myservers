cbd920385e1327b7aa52c3255cf1558e52eedf7c
ryuichi1208 <ryucrosskey@gmail.com>
2020-07-25 11:02:23 +0900

    fix ansible.cfg

---
+++ modules/__init__.py
@@ -0,0 +1,6 @@
+# import module snippets
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+from ansible.module_utils.pycompat24 import get_exception
+if __name__ == '__main__':
+    main()
